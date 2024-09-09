8e0d193f8279a0e04dfbab17be8bea4a22a7282d
profmanjupriya <mmuralidharanpriya@sdsu.edu>
2024-09-08 23:17:40 -0700

    First commit of azztec robotics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
+          className="dark"
+          src="/fulllogo.png"
+          alt="Aztec Robotics logo"
           width={180}
           height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            <p className="dark"> Read our docs</p>
           </a>
         </div>
       </main>
@@ -97,5 +97,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
